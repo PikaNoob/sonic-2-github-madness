b42738cf7c03ec2421a11c8019e213dd77634784
PikaNoob <87725268+PikaNoob@users.noreply.github.com>
2021-10-10 12:55:29 +0100

    invincible music

--- sound/s2/music/Invincible.asm
+++ sound/s2/music/Invincible.asm
@@ -1,128 +1,175 @@
-Invincible_Header:
-	smpsHeaderStartSong 2
-	smpsHeaderVoice     Invincible_Voices
+invbeta_Header:
+	smpsHeaderStartSong 1
+	smpsHeaderVoice     invbeta_Voices
 	smpsHeaderChan      $06, $03
-	smpsHeaderTempo     $01, $E8
+	smpsHeaderTempo     $01, $0B
 
-	smpsHeaderDAC       Invincible_DAC
-	smpsHeaderFM        Invincible_FM1,	$F4, $11
-	smpsHeaderFM        Invincible_FM2,	$F4, $09
-	smpsHeaderFM        Invincible_FM3,	$E8, $0F
-	smpsHeaderFM        Invincible_FM4,	$E8, $0F
-	smpsHeaderFM        Invincible_FM5,	$F4, $11
-	smpsHeaderPSG       Invincible_PSG1,	$F4, $02, $00, fTone_08
-	smpsHeaderPSG       Invincible_PSG2,	$DC, $05, $00, fTone_05
-	smpsHeaderPSG       Invincible_PSG3,	$00, $03, $00, fTone_04
+	smpsHeaderDAC       invbeta_DAC
+	smpsHeaderFM        invbeta_FM1,	$F4, $11
+	smpsHeaderFM        invbeta_FM2,	$F4, $09
+	smpsHeaderFM        invbeta_FM3,	$E8, $0F
+	smpsHeaderFM        invbeta_FM4,	$E8, $0F
+	smpsHeaderFM        invbeta_FM5,	$F4, $11
+	smpsHeaderPSG       invbeta_PSG1,	$F4, $02, $00, fTone_09
+	smpsHeaderPSG       invbeta_PSG2,	$DC, $05, $00, fTone_04
+	smpsHeaderPSG       invbeta_PSG3,	$00, $03, $00, fTone_04
 
 ; FM5 Data
-Invincible_FM5:
-	smpsAlterNote       $03
-
-; FM1 Data
-Invincible_FM1:
+invbeta_FM5:
 	dc.b	nRst, $30
+
+invbeta_Jump01:
+	smpsAlterNote       $03
 	smpsSetvoice        $00
 
-Invincible_Loop07:
+invbeta_Loop0C:
+	smpsCall            invbeta_Call00
+	smpsLoop            $00, $02, invbeta_Loop0C
+	smpsAlterNote       $00
+	smpsAlterVol        $FE
+	smpsSetvoice        $02
+	dc.b	nE6, $18, nE6, nE6, nE6, nFs6, $0C, nFs6, nFs6, nFs6, nA6, nA6
+	dc.b	nA6, nA6
+	smpsAlterVol        $02
+	smpsJump            invbeta_Jump01
+
+invbeta_Call00:
 	dc.b	nRst, $0C, nCs6, $15, nRst, $03, nCs6, $06, nRst, nD6, $0F, nRst
-	dc.b	$03, nB5, $18, nRst, $06, nCs6, $06, nRst, nCs6, nRst, nCs6, nRst
-	dc.b	nA5, nRst, nG5, $0F, nRst, $03, nB5, $18, nRst, $06
-	smpsLoop            $00, $02, Invincible_Loop07
-	smpsAlterVol        $FD
-	dc.b	nRst, $30, nRst, nA5, $04, nB5, nCs6, nD6, nE6, nFs6, nB5, nCs6
-	dc.b	nEb6, nE6, nFs6, nAb6, nCs6, nEb6, nF6, nFs6, nAb6, nBb6, nF6, nFs6
-	dc.b	nAb6, nBb6, nC7, nCs7
-	smpsAlterVol        $03
-	smpsJump            Invincible_Loop07
+	dc.b	$03, nB5, $18, nRst, $06, nCs6, nRst, nCs6, nRst, nCs6, nRst, nA5
+	dc.b	nRst, nG5, $0F, nRst, $03, nB5, $18, nRst, $06
+	smpsReturn
+
+; FM1 Data
+invbeta_FM1:
+	dc.b	nRst, $30
+
+invbeta_Jump00:
+	smpsSetvoice        $00
+
+invbeta_Loop0B:
+	smpsCall            invbeta_Call00
+	smpsLoop            $00, $02, invbeta_Loop0B
+	smpsSetvoice        $02
+	smpsAlterVol        $FE
+	dc.b	nA6, $18, nA6, nA6, nA6, nB6, $0C, nB6, nB6, nB6, nD7, nD7
+	dc.b	nD7, nD7
+	smpsAlterVol        $02
+	smpsJump            invbeta_Jump00
 
 ; FM2 Data
-Invincible_FM2:
+invbeta_FM2:
 	smpsNop             $01
 	smpsSetvoice        $01
 	dc.b	nRst, $30
 
-Invincible_Loop05:
+invbeta_Loop09:
 	dc.b	nA3, $06, nRst, nA3, nRst, nE3, nRst, nE3, nRst, nG3, $12, nFs3
 	dc.b	$0C, nG3, $06, nFs3, $0C, nA3, $06, nRst, nA3, nRst, nE3, nRst
 	dc.b	nE3, nRst, nD4, $12, nCs4, $0C, nD4, $06, nCs4, $0C
-	smpsLoop            $00, $02, Invincible_Loop05
+	smpsLoop            $00, $02, invbeta_Loop09
 
-Invincible_Loop06:
-	dc.b	nB2, $06, nG2, $12, nA2, $06, nRst, nB2, nRst
-	smpsLoop            $00, $02, Invincible_Loop06
-	dc.b	nA2, $0C, nB2, nCs3, nEb3, nB2, $06, nCs3, nEb3, nF3, nCs3, nEb3
-	dc.b	nF3, nFs3
+invbeta_Loop0A:
+	dc.b	nA3, $06, nFs3, $12, nG3, $06, nRst, nAb3, nRst
+	smpsLoop            $00, $02, invbeta_Loop0A
+	dc.b	nFs3, $18, nFs3, nE3, $0C, nE3, nE3, nE3
 	smpsNop             $01
-	smpsJump            Invincible_Loop05
+	smpsJump            invbeta_Loop09
 
 ; FM3 Data
-Invincible_FM3:
+invbeta_FM3:
 	smpsSetvoice        $00
 	dc.b	nRst, $30
 
-Invincible_Loop03:
+invbeta_Loop06:
 	dc.b	nE6, $06, nRst, nE6, nRst, nCs6, nRst, nCs6, nRst, nD6, $12, nFs6
 	dc.b	nA6, $0C, nE6, $06, nRst, nE6, nRst, nCs6, nRst, nCs6, nRst, nG6
-	dc.b	$12, nG6, $1E
-	smpsLoop            $00, $02, Invincible_Loop03
+	dc.b	$12, $1E
+	smpsLoop            $00, $02, invbeta_Loop06
 
-Invincible_Loop04:
-	dc.b	nRst, $06, nG5, $12, nA5, $06, nRst, $12
-	smpsLoop            $00, $04, Invincible_Loop04
-	smpsJump            Invincible_Loop03
+invbeta_Loop07:
+	dc.b	nRst, $06, nA6, $10, nRst, $02, nA6, $06, nRst, $12
+	smpsLoop            $00, $02, invbeta_Loop07
+	smpsNoteFill        $16
+	dc.b	nB6, $18, nB6
+	smpsNoteFill        $00
+	smpsAlterPitch      $0C
+	smpsAlterNote       $03
+
+invbeta_Loop08:
+	dc.b	nD7, $03
+	smpsAlterPitch      $FF
+	smpsLoop            $00, $10, invbeta_Loop08
+	smpsAlterPitch      $04
+	smpsAlterNote       $00
+	smpsJump            invbeta_Loop06
 
 ; FM4 Data
-Invincible_FM4:
+invbeta_FM4:
 	smpsSetvoice        $00
 	dc.b	nRst, $30
 
-Invincible_Loop01:
+invbeta_Loop03:
 	dc.b	nCs6, $06, nRst, nCs6, nRst, nA5, nRst, nA5, nRst, nB5, $12, nD6
 	dc.b	nFs6, $0C, nCs6, $06, nRst, nCs6, nRst, nA5, nRst, nA5, nRst, nD6
-	dc.b	$12, nD6, $1E
-	smpsLoop            $00, $02, Invincible_Loop01
+	dc.b	$12, $1E
+	smpsLoop            $00, $02, invbeta_Loop03
 
-Invincible_Loop02:
-	dc.b	nRst, $06, nB5, $12, nCs6, $06, nRst, $12
-	smpsLoop            $00, $04, Invincible_Loop02
-	smpsJump            Invincible_Loop01
+invbeta_Loop04:
+	dc.b	nRst, $06, nE6, $10, nRst, $02, nE6, $06, nRst, $12
+	smpsLoop            $00, $02, invbeta_Loop04
+	smpsNoteFill        $16
+	dc.b	nFs6, $18, nFs6
+	smpsNoteFill        $00
+	smpsAlterPitch      $0C
+
+invbeta_Loop05:
+	dc.b	nD7, $03
+	smpsAlterPitch      $FF
+	smpsLoop            $00, $10, invbeta_Loop05
+	smpsAlterPitch      $04
+	smpsJump            invbeta_Loop03
 
 ; PSG1 Data
-Invincible_PSG1:
+invbeta_PSG1:
 ; PSG2 Data
-Invincible_PSG2:
+invbeta_PSG2:
 	smpsStop
 
 ; PSG3 Data
-Invincible_PSG3:
+invbeta_PSG3:
 	smpsPSGform         $E7
 	dc.b	nRst, $30
 
-Invincible_Jump00:
+invbeta_Loop0D:
 	smpsNoteFill        $03
-	dc.b	nA5, $0C
+	dc.b	 $0C
 	smpsNoteFill        $0C
 	dc.b	$0C
-	smpsNoteFill        $03
-	dc.b	$0C
-	smpsNoteFill        $0C
-	dc.b	$0C
-	smpsJump            Invincible_Jump00
+	smpsLoop            $00, $02, invbeta_Loop0D
+	smpsJump            invbeta_Loop0D
 
 ; DAC Data
-Invincible_DAC:
-	dc.b	dSnare, $06, dSnare, dKick, dKick, dSnare, dSnare, dSnare, dSnare
+invbeta_DAC:
+	dc.b	dSnare, $06, dSnare, dKick, dKick, dSnare, dSnare, dSnare
 
-Invincible_Loop00:
-	dc.b	dKick, $0C, dSnare, dKick, dSnare, dKick, $0C, dSnare, dKick, dSnare, dKick, $0C
-	dc.b	dSnare, dKick, dSnare, dKick, $0C, dSnare, dKick, $04, nRst, dSnare, dSnare, $0C
-	smpsLoop            $00, $02, Invincible_Loop00
-	dc.b	dKick, $06, dSnare, $12, dKick, $0C, dSnare, dSnare, $06, dKick, $12, dKick
-	dc.b	$0C, dSnare, dSnare, $06, dKick, $0C, dSnare, $06, dKick, $0C, dSnare, dSnare
-	dc.b	$04, dSnare, dSnare, dSnare, dSnare, dSnare, dSnare, dSnare, dSnare, dSnare, dSnare, dSnare
-	smpsJump            Invincible_Loop00
+invbeta_Loop00:
+	dc.b	dSnare
 
-Invincible_Voices:
+invbeta_Loop01:
+	dc.b	dClap, $0C
+	smpsLoop            $00, $07, invbeta_Loop00
+	dc.b	dSnare, dKick, $04, nRst, dSnare, dSnare, $0C
+	smpsLoop            $01, $02, invbeta_Loop01
+	dc.b	dKick, $06, dSnare, $12, dKick, $0C, dSnare, dSnare, $06, dKick, $12, $0C
+	dc.b	dSnare, dSnare, $06, dKick, $0C, dSnare, $06, dKick, $0C
+
+invbeta_Loop02:
+	dc.b	dSnare, dSnare, $04
+	smpsLoop            $00, $06, invbeta_Loop02
+	dc.b	dSnare
+	smpsJump            invbeta_Loop01
+
+invbeta_Voices:
 ;	Voice $00
 ;	$3A
 ;	$01, $07, $01, $01, 	$8E, $8E, $8D, $53, 	$0E, $0E, $0E, $03
@@ -158,4 +205,22 @@ Invincible_Voices:
 	smpsVcDecayLevel    $0B, $0B, $05, $05
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
 	smpsVcTotalLevel    $00, $17, $2B, $14
+
+;	Voice $02
+;	$16
+;	$7A, $54, $3C, $31, 	$1F, $1F, $1F, $1F, 	$0A, $0C, $1C, $0C
+;	$07, $0C, $1C, $0C, 	$0F, $0F, $0F, $0F, 	$11, $85, $83, $80
+	smpsVcAlgorithm     $06
+	smpsVcFeedback      $02
+	smpsVcUnusedBits    $00
+	smpsVcDetune        $03, $03, $05, $07
+	smpsVcCoarseFreq    $01, $0C, $04, $0A
+	smpsVcRateScale     $00, $00, $00, $00
+	smpsVcAttackRate    $1F, $1F, $1F, $1F
+	smpsVcAmpMod        $00, $00, $00, $00
+	smpsVcDecayRate1    $0C, $1C, $0C, $0A
+	smpsVcDecayRate2    $0C, $1C, $0C, $07
+	smpsVcDecayLevel    $00, $00, $00, $00
+	smpsVcReleaseRate   $0F, $0F, $0F, $0F
+	smpsVcTotalLevel    $00, $03, $05, $11
 
